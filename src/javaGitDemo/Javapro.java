96cf3f17588230b27ccb06a2b1286ad6b160974b
kalpravi1989 <kalpravi1989@gmail.com>
2022-01-18 11:34:23 -0600

    first commit

---
+++ src/javaGitDemo/Javapro.java
@@ -0,0 +1,16 @@
+package javaGitDemo;
+
+public class Javapro {
+
+	public static void main(String[] args) {
+		int i=20;
+		int j=30;
+		if(i<j) {
+			System.out.println("the greatest number is"+j);
+		}else
+		{
+			System.out.println("the small number is"+i);
+		}
+	}
+
+}
