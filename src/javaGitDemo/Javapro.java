c67abaa4be52fa528154c896aef03d45fe971f0a
kalpravi1989 <kalpravi1989@gmail.com>
2022-01-18 11:35:00 -0600

    second commit

--- src/javaGitDemo/Javapro.java
+++ src/javaGitDemo/Javapro.java
@@ -5,11 +5,15 @@ public class Javapro {
 	public static void main(String[] args) {
 		int i=20;
 		int j=30;
-		if(i<j) {
+		int k=40;
+		if(j>i && j>k) {
 			System.out.println("the greatest number is"+j);
+		}else if(i>j && i>k)
+		{
+			System.out.println("the greatest number is"+i);
 		}else
 		{
-			System.out.println("the small number is"+i);
+			System.out.println("the greatest number is"+k);
 		}
 	}
 
